0c1dcc5593a4a906f188d35be4e6be924b5c05fd
Felix <felix.betzl@feki.de>
2018-04-20 09:57:40 +0200

    initial commit

---
+++ MineSweeper.ts
@@ -0,0 +1,340 @@
+class Startup {
+    public static main(): any {
+        var MineSweeper = new Game();
+
+        $(document).ready(function(){
+            $('#board').on('mouseup', '.field', function (e) {                
+                var x = $(this).data("x");
+                var y = $(this).data("y");
+                switch (e.which)
+                {
+                    // Left Click.
+                    case 1:
+                        MineSweeper.step_on_field(x, y);
+                        MineSweeper.check_victory();
+                    break;            
+                    // Middle click.
+                    case 2:
+                        MineSweeper.mark_field(x, y);
+                    break;
+                }
+                var number_of_flags = MineSweeper.Board.count_flags();
+                document.getElementById('number-of-flags').innerHTML = number_of_flags.toString();                
+                return true;
+            });
+        });        
+    }
+}
+
+class Game {
+    public Board : Board;
+    public Countdown : Countdown;
+    constructor() {
+        this.Board = new Board(16, 30, 99);
+        this.Countdown = new Countdown();
+        this.Countdown.countdown("time", 0, 30);
+    }
+
+    public step_on_field(x: number, y: number) : void {
+        try {
+            this.Board.step_on_field(x, y);
+            if(this.check_victory()) {
+                console.log("SIEG!");
+            }            
+        } catch(err) {
+            this.Board.cheat();
+            this.Board.fields[x][y].view = FieldView.Exploding;
+            this.Board.printBoard();
+            console.log("Leider verloren, zahl deine Steuern!");
+        }
+    }
+
+    public mark_field(x: number, y: number) : void {
+        this.Board.markField(x, y);
+    }
+
+    public check_victory() : boolean {
+        var visited_fields = 0;
+        var everything_visited = false;
+        var fields = this.Board.sizeY * this.Board.sizeX;
+
+        for(var i = 0; i < this.Board.sizeX; i++) {
+            for(var j = 0; j < this.Board.sizeY; j++) {
+                if(this.Board.fields[i][j].visited) {
+                    visited_fields++;
+                }
+            }
+        }
+
+        if(fields - visited_fields == this.Board.number_of_bombs) {
+            everything_visited = true;
+        }
+
+        return everything_visited;
+    }
+}
+
+class Countdown {
+    constructor() {
+
+    }
+
+    public countdown( elementName, minutes, seconds ) : void
+    {
+        var element, endTime, hours, mins, msLeft, time;
+    
+        function twoDigits( n )
+        {
+            return (n <= 9 ? "0" + n : n);
+        }
+    
+        function updateTimer()
+        {
+            msLeft = endTime + (+new Date);
+            if ( msLeft < 1000 ) {
+                element.innerHTML = "countdown's over!";
+            } else {
+                time = new Date( msLeft );
+                hours = time.getUTCHours();
+                mins = time.getUTCMinutes();
+                element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
+                setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+            }
+        }
+    
+        element = document.getElementById( elementName );
+        endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+        updateTimer();
+    }
+}
+
+class Board {
+    public fields : Field[][];
+    public sizeX : number;
+    public sizeY: number;
+    public number_of_bombs : number;
+
+    constructor(sizeX : number, sizeY : number, number_of_bombs : number) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        this.number_of_bombs = number_of_bombs;
+        this.createFields();
+        this.seedMines();
+        this.printBoard();
+    }
+
+    public count_flags() {
+        var flags = 0;
+
+        for(var i = 0; i < this.sizeX; i++) {
+            for(var j = 0; j < this.sizeY; j++) {
+                if(this.fields[i][j].view == FieldView.Flagged) {
+                    flags++;
+                }
+            }
+        }
+        
+        return flags;        
+    }
+
+    public cheat() : void {
+        for(var i = 0; i < this.sizeX; i++) {
+            for(var j = 0; j < this.sizeY; j++) {
+                if(this.fields[i][j].type == FieldType.Mine) {
+                    this.fields[i][j].view = FieldView.Mine;
+                } else {
+                    this.fields[i][j].view = this.calculate_field_view(i, j);
+                }
+            }
+        }
+        this.printBoard();
+    }
+
+    public markField(x : number, y : number) : void {
+        this.fields[x][y].set_next_marker();
+        this.printBoard();
+    }
+
+    public step_on_field(x: number, y : number) : void {
+        if(this.fields[x][y].view == FieldView.Flagged) {
+            return;
+        }
+
+        if(this.fields[x][y].type == FieldType.Mine) {
+            this.fields[x][y].view = FieldView.Exploding;
+            throw 1;
+        }
+        else {
+            this.calculate_field_view(x, y);
+        }
+        this.printBoard();
+    }
+
+    private calculate_field_view(x : number, y : number) : FieldView {
+        var neighbor_mines : number = 0;
+        var field_view : FieldView;
+        neighbor_mines = this.findingNeighbors(x, y);
+
+        switch(neighbor_mines) {
+            case 1:
+                field_view =  FieldView.One;
+            break;
+            case 2:
+                field_view =  FieldView.Two;
+            break;
+            case 3:
+                field_view =  FieldView.Three;
+            break;
+            case 4:
+                field_view =  FieldView.Four;
+            break;
+            case 5:
+                field_view =  FieldView.Five;
+            break;
+            case 6:
+                field_view =  FieldView.Six;
+            break;
+            case 7:
+                field_view =  FieldView.Seven;
+            break;
+            case 8:
+                field_view =  FieldView.Eight;
+            break;
+            default:
+                field_view =  FieldView.Zero;
+            break;                                                                                              
+        }
+
+        this.fields[x][y].view = field_view;
+        this.fields[x][y].visited = true;
+
+        if(neighbor_mines == 0) {
+            this.step_on_neighbours(x, y);
+        }
+
+        return field_view;
+    }
+
+    private findingNeighbors(i, j) : number {
+        var rowLimit = this.sizeX-1;
+        var columnLimit = this.sizeY-1;
+        var neighbor_mines = 0;
+      
+        for(var x = Math.max(0, i-1); x <= Math.min(i+1, rowLimit); x++) {
+          for(var y = Math.max(0, j-1); y <= Math.min(j+1, columnLimit); y++) {
+            if(x !== i || y !== j) {
+              if(this.fields[x][y].type == FieldType.Mine) {
+                  neighbor_mines++;
+              }
+            }
+          }
+        }
+        return neighbor_mines;
+    }
+
+    private step_on_neighbours(i, j) : void {
+        var rowLimit = this.sizeX-1;
+        var columnLimit = this.sizeY-1;
+      
+        for(var x = Math.max(0, i-1); x <= Math.min(i+1, rowLimit); x++) {
+          for(var y = Math.max(0, j-1); y <= Math.min(j+1, columnLimit); y++) {
+            if(x !== i || y !== j) {
+                if(this.fields[x][y].view == FieldView.Unknown) {
+                    this.calculate_field_view(x, y);
+                }
+            }
+          }
+        }      
+    }
+
+    private createFields() : void {
+        this.fields = [];
+        for(var i = 0; i < this.sizeX; i++) {
+            this.fields[i] = [];
+            for(var j = 0; j < this.sizeY; j++) {
+                this.fields[i][j] = new Field();
+            }
+        }
+    }
+
+    private seedMines() : void {
+        var bombsPlaced = 0;
+        while(bombsPlaced < this.number_of_bombs) {
+            var randX = Math.floor((Math.random() * this.sizeX));
+            var randY = Math.floor((Math.random() * this.sizeY));
+            if(this.fields[randX][randY].type == FieldType.Empty) {
+                this.fields[randX][randY].type = FieldType.Mine;
+                bombsPlaced++;
+            }
+        }
+    }
+
+    public printBoard() : void {
+        var html = this.to_html();
+        document.getElementById('board').innerHTML = html;
+    }
+
+    private to_html() : string {
+        var html = '';
+        for(var i = 0; i < this.sizeX; i++) {
+            html += '<div class="row">';
+            for(var j = 0; j < this.sizeY; j++) {
+                html += this.fields[i][j].to_html(i, j);
+            }
+            html += '</div>';
+        }
+
+        return html;
+    }
+}
+
+class Field {
+    public type : FieldType;
+    public view : FieldView;
+    public visited : boolean;
+
+    constructor() {
+        this.view = FieldView.Unknown;
+        this.type = FieldType.Empty;
+        this.visited = false;
+    }
+
+    public set_next_marker() : void {
+        switch(this.view) {
+            case FieldView.Unknown:
+                this.view = FieldView.Flagged;
+            break;
+            case FieldView.Flagged:
+                this.view = FieldView.Unknown;
+            break;
+        }
+    }
+
+    public to_html(x : number, y : number) : string {
+        var html = '<div class="field ' + FieldView[this.view] + '" data-x="' + x + '" data-y="' + y + '"></div>';
+        return html;
+    }
+}
+
+enum FieldType {
+    Mine,
+    Empty
+}
+
+enum FieldView {
+    Unknown,
+    Flagged,
+    Exploding,
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Mine
+}
+
+Startup.main();
